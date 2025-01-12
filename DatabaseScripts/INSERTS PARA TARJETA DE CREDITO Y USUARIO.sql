837f54c4a1a8a70998a9c694cd4ff7d8e2d6cd5b
LUIS ANTONIO GUZMAN ESCOBAR <76543525+LuisGuzman2535@users.noreply.github.com>
2025-01-11 22:53:47 -0600

    Update INSERTS PARA TARJETA DE CREDITO Y USUARIO.sql

--- DatabaseScripts/INSERTS PARA TARJETA DE CREDITO Y USUARIO.sql
+++ DatabaseScripts/INSERTS PARA TARJETA DE CREDITO Y USUARIO.sql
@@ -6,4 +6,4 @@ INSERT INTO Usuarios VALUES ('Luis Guzman', 'luis@gmail.com')
 INSERT INTO Usuarios VALUES ('David Castro', 'david@gmail.com')
 
 INSERT INTO TarjetasDeCredito  VALUES(1,'4365878599258755', 2000, 0)
-INSERT INTO TarjetasDeCredito  VALUES(2,'4365878599258755', 2000, 0)
+INSERT INTO TarjetasDeCredito  VALUES(2,'4365878599258756', 2000, 0)
