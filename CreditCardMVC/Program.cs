e1f4f1c118e877d5af2013013066a3f027331980
Luis Antonio Guzman Escobar <snowerluis01@outlook.com>
2025-01-11 22:16:18 -0600

    Mejoras en pantalla de transacciones

--- CreditCardMVC/Program.cs
+++ CreditCardMVC/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllersWithViews();
 // Establece la dirección base para el cliente HTTP
 builder.Services.AddHttpClient("CreditCardAPI", client =>
 {
+    //La uri podría ser https://localhost:7067  ó http://localhost:5008 dependiendo de 
+    //como se tenga configurado en la dirección URL de la aplicación CreditCardAPI
     client.BaseAddress = new Uri("https://localhost:7067/");
 });
 
